e994145d87508dd06e69ebe94cd1b7554e09be61
Dmitry Obukhov <stel2k@gmail.com>
2013-04-25 22:55:41 +0400

    Appledoc-style comments

--- DOSingleton/DOSingleton.h
+++ DOSingleton/DOSingleton.h
@@ -8,8 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ * `DOSingleton` is a generic class for Mac OSX and iOS that implements all the required methods to 
+ * implement a singleton object. It's designed for subclassing.
+ *
+ * When subclassing `DOSingleton` you should think about your class as an ordinary class, `DOSingleton`
+ * makes sure that there is only one instance of your class.
+ */
+
 @interface DOSingleton : NSObject
 
+/**
+ * @name Instantiating
+ */
+
+/**
+ * Returns the singleton object of the receiver class.
+ *
+ * You shoudn't override this method in your subclass.
+ *
+ * @return Singleton object of the receiver class.
+ */
 + (instancetype)sharedInstance;
 
 @end
--- DOSingleton/DOSingleton.m
+++ DOSingleton/DOSingleton.m
@@ -8,6 +8,7 @@
 
 #import "DOSingleton.h"
 
+// Dictionary that holds all instances of DOSingleton subclasses
 static NSMutableDictionary *_sharedInstances = nil;
 
 @implementation DOSingleton
@@ -26,8 +27,10 @@ static NSMutableDictionary *_sharedInstances = nil;
 	@synchronized(self) {
 		NSString *instanceClass = NSStringFromClass(self);
 		
+		// Looking for existing instance
 		sharedInstance = [_sharedInstances objectForKey:instanceClass];
 		
+		// If there's no instance – create one and add it to the dictionary
 		if (sharedInstance == nil) {
 			sharedInstance = [[super allocWithZone:nil] init];
 			[_sharedInstances setObject:sharedInstance forKey:instanceClass];
@@ -39,16 +42,19 @@ static NSMutableDictionary *_sharedInstances = nil;
 
 + (id)allocWithZone:(NSZone *)zone
 {
+	// Not allow allocating memory in a different zone
 	return [self sharedInstance];
 }
 
 + (id)copyWithZone:(NSZone *)zone
 {
+	// Not allow copying to a different zone
 	return [self sharedInstance];
 }
 
 - (id)init
 {
+	// Looking for existing instance
 	id sharedInstance = [_sharedInstances objectForKey:NSStringFromClass([self class])];
 	
 	if (sharedInstance != nil) {
